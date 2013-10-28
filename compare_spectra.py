1dab3785b40493e53c1b18dfeb94b9a3ce1e0eed
Ryan M. Bergmann <rmb@berkeley.edu>
2013-10-28 13:41:21 +0100

    added serpent parse to spectra plotting script

--- compare_spectra.py
+++ compare_spectra.py
@@ -1,36 +1,49 @@
 import pylab
 import sys
 import os
+import re
 
-def get_serpent_spec():
+def get_serpent_det(filepath):
+	fobj    = open(filepath)
+	fstr    = fobj.read()
+	names   = re.findall('[a-zA-Z]+ *= *\[',fstr)
+	data    = re.findall('\[ *\n[\w\s+-.]+\];',fstr)
+	alldata = dict()
+	dex     = 0
+	for name in names:
+		varname  = name.split()[0]
+		moredata = re.findall(' [ .+-eE0-9^\[]+\n',data[dex])
+		thisarray = numpy.array([])
+		for lines in moredata:
+			thisarray=numpy.append(thisarray,numpy.array(moredata[1].split(),dtype=float),0)
+		alldata[varname]=thisarray
+		dex = dex + 1
+	return alldata
 
 
 if   case=='water':
-	tally      = numpy.loadtext('water.tally')
-	tallybins  = numpy.loadtext('water.tallybins')
-	run ../serpent-benchmark/nonfiss_mono2_det0.m
+	tally      = numpy.loadtxt('water.tally')
+	tallybins  = numpy.loadtxt('water.tallybins')
+	serpdata   = get_serpent_det('..serpent-benchmark/nonfiss_mono2_det0.m')
 elif case== 'carbon':
-	tally      = numpy.loadtext('carbon.tally')
-	tallybins  = numpy.loadtext('carbon.tallybins')
-	run ../serpent-benchmark/carbon_mono2_det0.m
+	tally      = numpy.loadtxt('carbon.tally')
+	tallybins  = numpy.loadtxt('carbon.tallybins')
+	serpdata   = get_serpent_det('../serpent-benchmark/carbon_mono2_det0.m')
 elif case== 'lithium':
-	tally      = numpy.loadtext('lithium.tally')
-	tallybins  = numpy.loadtext('lithium.tallybins')
-	#mcnp_tally = numpy.loadtext('../mcnp-benchmark/lithium.tally')
-	run ../serpent-benchmark/lithium_mono2_det0.m
+	tally      = numpy.loadtxt('lithium.tally')
+	tallybins  = numpy.loadtxt('lithium.tallybins')
+	serpdata   = get_serpent_det('../serpent-benchmark/lithium_mono2_det0.m')
 elif case== 'hydrogen':
-	tally      = numpy.loadtext('hydrogen.tally')
-	tallybins  = numpy.loadtext('hydrogen.tallybins')
-	run ../serpent-benchmark/hydrogen_mono2_det0.m
+	tally      = numpy.loadtxt('hydrogen.tally')
+	tallybins  = numpy.loadtxt('hydrogen.tallybins')
+	serpdata   = get_serpent_det('../serpent-benchmark/hydrogen_mono2_det0.m')
 elif case== 'hydrogen2':
-	tally      = numpy.loadtext('hydrogen2.tally')
-	tallybins  = numpy.loadtext('hydrogen2.tallybins')
-	run ../serpent-benchmark/hydrogen2_mono2_det0.m
+	tally      = numpy.loadtxt('hydrogen2.tally')
+	tallybins  = numpy.loadtxt('hydrogen2.tallybins')
+	serpdata   = get_serpent_det('../serpent-benchmark/hydrogen2_mono2_det0.m')
 
 plot_spectra_serpent(DETfluxlogE,DETfluxlog)
-hold on
 plot_spectra_warp(tally,tallybins,'r')
-#plot_spectra_mcnp(mcnp_tally(:,2),mcnp_tally(:,1),'k')
 
 set(gca,'FontSize',14)
 title({'Serpent2 (Serial) vs. WARP','4e6 histories, 2MeV point source at origin of 84x84x84cm water block'})
