d3fa96cb2b9b2bbc381e7be6c69237f20d8f449c
Ryan M. Bergmann <rmb@berkeley.edu>
2014-03-19 12:44:00 -0700

    it works it works hallelujah it works!!!!!!!

--- fission_source.py
+++ fission_source.py
@@ -9,10 +9,10 @@ data=np.reshape(data,(-1,3))
 
 fig = pl.figure(figsize=(10,6))
 ax = fig.add_subplot(1,1,1)
-xmin = ymin = -2
-xmax = ymax =  2
-ax.hist2d(data[:,0], data[:,1], range=[[xmin, xmax], [ymin, ymax]], bins=256 , normed=True)#norm=LogNorm())
-fig.colorbar(ax.get_images()[0], ax=ax, ticks=[ 0.0, 0.5, 1.0], cmap=pl.cm.jet, label='Relative Probability')  #, norm=pl.matplotlib.colors.Normalize(vmin=5, vmax=10))
+xmin = ymin = -40
+xmax = ymax =  40
+ax.hist2d(data[:,0], data[:,1], range=[[xmin, xmax], [ymin, ymax]], bins=512 , normed=True)#norm=LogNorm())
+fig.colorbar(ax.get_images()[0], ax=ax, ticks=np.linspace(0,1,11), cmap=pl.cm.jet, label='Relative Probability')  #, norm=pl.matplotlib.colors.Normalize(vmin=5, vmax=10))
 ax.set_xlabel('x (cm)')
 ax.set_ylabel('y (cm)')
 ax.grid('on',color='k')
